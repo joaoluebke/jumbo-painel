6e236e471429669c2f17a2155e74d2e131a67410
João Luca Amorim Luebke <57159653+joaoluebke@users.noreply.github.com>
2023-01-05 17:55:12 -0300

    https

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,7 +4,7 @@ import { parseCookies } from "nookies";
 const { "jumbo-token": token } = parseCookies();
 
 export const api = axios.create({
-  baseURL: 'http://15.228.222.52:3333',
+  baseURL: 'https://15.228.222.52:3333',
 });
 
 if (token) {
